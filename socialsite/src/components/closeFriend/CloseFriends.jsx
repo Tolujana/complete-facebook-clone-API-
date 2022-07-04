8cef726ef16028a433c6aada4a79dd1f8722e1d3
Tolu Olofinjana <Tolujana@gmail.com>
2022-07-04 22:14:50 +0100

    fixed images

--- socialsite/src/components/closeFriend/CloseFriends.jsx
+++ socialsite/src/components/closeFriend/CloseFriends.jsx
@@ -1,15 +1,23 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import styles from "./CloseFriends.module.css";
-const PF = process.env.REACT_APP_EXTERNAL_FOLDER
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styles from './CloseFriends.module.css';
+const PF = process.env.REACT_APP_EXTERNAL_FOLDER;
 
 const CloseFriends = ({ user }) => {
   return (
-  <Link style={{textDecoration:"none"} }to= {"/profile/"+user.username}>
-    <li className={styles.leftbarFriend}>
-      <img src={user.profilePicture?PF+user.profilePicture:PF+"assets/noimage.png"} alt="" className={styles.friendImg} />
-      <span className={styles.friendName}> {user.username}</span>
-    </li>
+    <Link style={{ textDecoration: 'none' }} to={'/profile/' + user.username}>
+      <li className={styles.leftbarFriend}>
+        <img
+          src={
+            user.profilePicture
+              ? PF + user.profilePicture
+              : PF + 'assets/noimage.png'
+          }
+          alt=""
+          className={styles.friendImg}
+        />
+        <span className={styles.friendName}> {user.username}</span>
+      </li>
     </Link>
   );
 };
