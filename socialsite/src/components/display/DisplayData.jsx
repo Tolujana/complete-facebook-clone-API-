967971c92585fe7506f145e59c71b72fa38037fb
unknown <tolujana@yahoo.com>
2023-03-31 14:51:26 +0100

    redesigned drag and drop

--- socialsite/src/components/display/DisplayData.jsx
+++ socialsite/src/components/display/DisplayData.jsx
@@ -1,19 +1,11 @@
 import React, { useContext } from "react";
 import "./display.css";
 import { AuthContext } from "../../context/AuthContext";
-const DisplayData = ({ files, design }) => {
-  //   const { user, post } = useContext(AuthContext);
-  //   const designType = ["row", "column"];
-  //   const randomNumber = Math.floor(Math.random() * 2);
-  //   const classNameOptions = ["one", "two", "three", "four"];
-  //   const numberOfFiles = files.length;
-  //   const cssName =
-  //     numberOfFiles < 5 ? classNameOptions[numberOfFiles - 1] : "multiple";
-  //   console.log(cssName);
+const DisplayData = ({ files, cssName }) => {
   const EXTERNAL_FOLDER = process.env.REACT_APP_EXTERNAL_FOLDER;
-  console.log(files);
+
   return (
-    <div className={`container ${design}`}>
+    <div className={`container ${cssName}`}>
       {typeof files === "string" ? (
         <div className="image">
           <img src={EXTERNAL_FOLDER + files} alt="" className="grid-image" />
@@ -23,7 +15,11 @@ const DisplayData = ({ files, design }) => {
           return (
             <div className="image">
               <img
-                src={URL.createObjectURL(file) || EXTERNAL_FOLDER + file}
+                src={
+                  cssName.includes("post")
+                    ? EXTERNAL_FOLDER + file
+                    : URL.createObjectURL(file)
+                }
                 alt=""
                 className="grid-image"
               />
