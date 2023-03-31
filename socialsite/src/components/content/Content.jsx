4d0f6adaf0319ac4e2cfc87abf8ae23a6415f397
unknown <tolujana@yahoo.com>
2023-03-31 14:48:54 +0100

    added infinite scroll for timeline and homepage

--- socialsite/src/components/content/Content.jsx
+++ socialsite/src/components/content/Content.jsx
@@ -6,29 +6,42 @@ import axios from "axios";
 import { AuthContext } from "../../context/AuthContext";
 import { axiosInstance } from "../../proxySettings";
 import Story from "../story/Story";
+let offset = 0;
 
 function Content({ username }) {
   const [posts, setPost] = useState([]);
   const { user } = useContext(AuthContext);
+  const fetchPost = async (offset) => {
+    const res = username
+      ? await axiosInstance.get(
+          "/posts/profile/" + username + "?offset=" + offset
+        )
+      : await axiosInstance.get(
+          `posts/timeline/${user._id}` + "?offset=" + offset
+        );
+    const sortedPost = res.data.sort((p1, p2) => {
+      return new Date(p2.createdAt) - new Date(p1.createdAt);
+    });
+    setPost((oldPost) => [...oldPost, ...sortedPost]);
+  };
 
+  const loadmore = (e) => {
+    const scrollHeight = e.target.documentElement.scrollHeight;
+    const scrollTop = e.target.documentElement.scrollTop;
+    if (window.innerHeight + scrollTop + 7 >= scrollHeight) {
+      offset += 3;
+      fetchPost(offset);
+    }
+  };
   useEffect(() => {
-    const fetchPost = async () => {
-      const res = username
-        ? await axiosInstance.get("/posts/profile/" + username)
-        : await axiosInstance.get(`posts/timeline/${user._id}`);
-
-      setPost(
-        res.data.sort((p1, p2) => {
-          return new Date(p2.createdAt) - new Date(p1.createdAt);
-        })
-      );
-    };
-    fetchPost();
+    fetchPost(0);
+    window.addEventListener("scroll", loadmore);
   }, [username, user._id]);
+
   return (
     <div className={style.content}>
       <div className={style.wrapper}>
-        {username === user.username && (
+        {username !== user.username && (
           <>
             <Story />
             <Share />
