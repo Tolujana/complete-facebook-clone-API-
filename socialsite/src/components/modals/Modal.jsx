23b9f2f1642cfa2bb9aa7eb9f58b4feb66ab05a4
unknown <tolujana@yahoo.com>
2023-04-17 02:45:17 +0100

    added close button for modal

--- socialsite/src/components/modals/Modal.jsx
+++ socialsite/src/components/modals/Modal.jsx
@@ -1,21 +1,59 @@
-import React from "react";
+import React, { useContext } from "react";
 import styles from "./modal.module.css";
 import SharePopup from "../sharePopup/SharePopup";
+import CommentPopup from "../commentPopup/CommentPopup";
+import CloseIcon from "@mui/icons-material/Close";
+import { AuthContext } from "../../context/AuthContext";
+import { openPopupDialog } from "../../utils/generalServices";
 
 const Modal = ({ type }) => {
-  return type === "register" ? (
-    <div className={styles.fullScreenModal}></div>
-  ) : type === "share" ? (
-    <div className={styles.fullScreenModal}>
-      <SharePopup />
-    </div>
-  ) : type === "comment" ? (
-    <div className={styles.fullScreenModal}>
-      <commentPopup />
-    </div>
-  ) : (
-    ""
+  const { dispatch } = useContext(AuthContext);
+  const action = { type: "MODAL_TYPE", payload: "" };
+  const getComponentToDisplay = (type) => {
+    switch (type.name) {
+      case "register":
+        return <div className={styles.fullScreenModal}></div>;
+      case "share":
+        return <SharePopup />;
+      case "comment":
+        return <CommentPopup id={type.id} />;
+      default:
+        return "";
+    }
+  };
+
+  const Component = getComponentToDisplay(type);
+  const disableModal = () => {
+    openPopupDialog(action, dispatch);
+  };
+  return (
+    <>
+      {type !== "" && (
+        <div className={styles.fullScreenModal}>
+          <div className={styles.container}>
+            <div className={styles.close} onClick={disableModal}>
+              <CloseIcon />
+            </div>
+            {Component}
+          </div>
+        </div>
+      )}
+    </>
   );
+
+  // return type === "register" ? (
+  //   <div className={styles.fullScreenModal}></div>
+  // ) : type === "share" ? (
+  //   <div className={styles.fullScreenModal}>
+  //     <SharePopup />
+  //   </div>
+  // ) : type === "comment" ? (
+  //   <div className={styles.fullScreenModal}>
+  //     <CommentPopup />
+  //   </div>
+  // ) : (
+  //   ""
+  // );
 };
 
 export default Modal;
