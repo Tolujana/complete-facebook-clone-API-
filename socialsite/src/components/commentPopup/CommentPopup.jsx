b40cb3c95ddc20334ac3b331f6a30fe23f25ac06
unknown <tolujana@yahoo.com>
2023-04-18 21:49:44 +0100

    added user post to comment popup

--- socialsite/src/components/commentPopup/CommentPopup.jsx
+++ socialsite/src/components/commentPopup/CommentPopup.jsx
@@ -1,7 +1,13 @@
 import React from "react";
-
-const CommentPopup = () => {
-  return <div>CommentPopupsssssss</div>;
+import PostInComment from "../post/PostInComment";
+import styles from "./commentPopup.module.css";
+const CommentPopup = ({ post, user }) => {
+  return (
+    <div className={styles.commentContainer}>
+      <div className={styles.title}>{`${user}'s Post`}</div>
+      <PostInComment post={post} />
+    </div>
+  );
 };
 
 export default CommentPopup;
