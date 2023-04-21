47adc70ad56648700b46f9239d981599abd70bcb
unknown <tolujana@yahoo.com>
2023-04-21 15:38:52 +0100

    api for comments added

--- route/posts.js
+++ route/posts.js
@@ -64,14 +64,18 @@ router.put("/:id/like", async (req, res) => {
 router.put("/:id/comment", async (req, res) => {
   try {
     const post = await Post.findById(req.params.id);
-    if (!post.likes.includes(req.body.userId)) {
-      await post.updateOne({ $push: { likes: req.body.userId } });
 
-      res.status(200).json("post has been liked");
-    } else {
-      await post.updateOne({ $pull: { likes: req.body.userId } });
-      res.status(200).json("post has been dislike");
-    }
+    await post.updateOne({
+      $push: {
+        comment: {
+          userId: req.body.userId,
+          comment: req.body.comment,
+          comments: [],
+        },
+      },
+    });
+    console.log(req.body.comment);
+    res.status(200).json("post commented");
   } catch (error) {
     res.status(403).json(error);
   }
