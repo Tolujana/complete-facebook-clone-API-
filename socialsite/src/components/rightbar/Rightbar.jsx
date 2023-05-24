c4199054e99d31fc3f9bcd1d6ad7ce543a1a14a2
user <tolujana@gmail.com>
2023-05-24 13:53:48 +0100

    made right bar sticky

--- socialsite/src/components/rightbar/Rightbar.jsx
+++ socialsite/src/components/rightbar/Rightbar.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import styles from './rightbar.module.css';
-import VideoCallIcon from '@mui/icons-material/VideoCall';
-import SearchIcon from '@mui/icons-material/Search';
+import React from "react";
+import styles from "./rightbar.module.css";
+import VideoCallIcon from "@mui/icons-material/VideoCall";
+import SearchIcon from "@mui/icons-material/Search";
 
-import MoreMenu from '../moreMenu/MoreMenu';
-import GroupIcon from '@mui/icons-material/Group';
-import { Users } from '../../dummyData';
-import { FriendsOnline } from '../friends/FriendsOnline';
+import MoreMenu from "../moreMenu/MoreMenu";
+import GroupIcon from "@mui/icons-material/Group";
+import { Users } from "../../dummyData";
+import { FriendsOnline } from "../friends/FriendsOnline";
 const Folder = process.env.REACT_APP_PUBLIC_FOLDER;
 
 function Rightbar() {
@@ -27,26 +27,14 @@ function Rightbar() {
           <div className={styles.middle}>
             <div className={styles.middleTop}>
               <div className={styles.imgdiv}>
-                <img
-                  src="images/persons/2.jpg"
-                  alt=""
-                  className={styles.friend}
-                />
+                <img src="images/persons/2.jpg" alt="" className={styles.friend} />
               </div>
 
               <div className={styles.info}>
                 <span className={styles.friendName}> DamiLola Olofinjana</span>
                 <div className={styles.imgMutualFriends}>
-                  <img
-                    src="images/persons/1.jpg"
-                    alt=""
-                    className={styles.imgMutualFriend}
-                  />
-                  <img
-                    src="images/persons/1.jpg"
-                    alt=""
-                    className={styles.imgMutualFriend}
-                  />
+                  <img src="images/persons/1.jpg" alt="" className={styles.imgMutualFriend} />
+                  <img src="images/persons/1.jpg" alt="" className={styles.imgMutualFriend} />
                   <span className={styles.mutualFriend}>9mutual friends</span>
                 </div>
               </div>
@@ -55,32 +43,33 @@ function Rightbar() {
           </div>
         </div>
       </div>
-
-      <hr className={styles.line} />
-      <div className={styles.sponsored}>
-        <span className={styles.title}>Sponsored</span>
-        <div className={styles.Rightads}>
-          <img src="images/ads/1.jpg" alt="" className={styles.Rightad} />
-        </div>
-        <div className={styles.Rightads}>
-          <img src="images/ads/1.jpg" alt="" className={styles.Rightad} />
-        </div>
-      </div>
-      <hr className={styles.line} />
-      <div className={styles.contactDetail}>
-        <div className={styles.contactstitle}>
-          <span className={styles.title}>Contacts</span>
-          <div className={styles.contactButtons}>
-            <VideoCallIcon className={styles.videoCall} />
-            <SearchIcon className={styles.Search} />
-            <MoreMenu />
+      <div className={styles.rightSticky}>
+        <hr className={styles.line} />
+        <div className={styles.sponsored}>
+          <span className={styles.title}>Sponsored</span>
+          <div className={styles.Rightads}>
+            <img src="images/ads/1.jpg" alt="" className={styles.Rightad} />
+          </div>
+          <div className={styles.Rightads}>
+            <img src="images/ads/1.jpg" alt="" className={styles.Rightad} />
           </div>
         </div>
-        <ul className={styles.friendsList}>
-          {Users.map((u) => (
-            <FriendsOnline key={u.id} user={u} />
-          ))}
-        </ul>
+        <hr className={styles.line} />
+        <div className={styles.contactDetail}>
+          <div className={styles.contactstitle}>
+            <span className={styles.title}>Contacts</span>
+            <div className={styles.contactButtons}>
+              <VideoCallIcon className={styles.videoCall} />
+              <SearchIcon className={styles.Search} />
+              <MoreMenu />
+            </div>
+          </div>
+          <ul className={styles.friendsList}>
+            {Users.map((u) => (
+              <FriendsOnline key={u.id} user={u} />
+            ))}
+          </ul>
+        </div>
       </div>
     </div>
   );
