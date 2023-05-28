21ac6e096c04e6ebcd534c4e63d93e0824c4283b
user <tolujana@gmail.com>
2023-05-28 22:39:00 +0100

    updated story component

--- socialsite/src/components/story/Story.jsx
+++ socialsite/src/components/story/Story.jsx
@@ -5,6 +5,7 @@ import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 import { removeButton, currentElement } from "../../utils/storyServices";
 let scrollAmount = 0;
+const PUBLIC_FOLDER = process.env.REACT_APP_PUBLIC_FOLDER;
 const Story = () => {
   const stories = useRef();
   const [scrollState, setScrollState] = useState();
@@ -21,8 +22,7 @@ const Story = () => {
         behavior: "smooth",
       });
     } else {
-      scrollAmount =
-        scrollAmount > maxScroll ? maxScroll : (scrollAmount += 112.5);
+      scrollAmount = scrollAmount > maxScroll ? maxScroll : (scrollAmount += 112.5);
       element.scrollTo({
         top: 0,
         left: scrollAmount,
@@ -33,8 +33,7 @@ const Story = () => {
   }
 
   const displayNoneleft = scrollState?.[0] < 10 ? styles.displayNone : "";
-  const displayNoneRight =
-    scrollState?.[0] > scrollState?.[1] - 500 ? styles.displayNone : "";
+  const displayNoneRight = scrollState?.[0] > scrollState?.[1] - 500 ? styles.displayNone : "";
 
   return (
     <div className={styles.stories}>
@@ -46,35 +45,56 @@ const Story = () => {
           <ArrowBackIosIcon className={styles.buttons} />
         </div>
         <div className={styles.storyContainer}>
-          <div className={styles.story}>
-            <div className={styles.profilePics}>
-              <img src="" alt="" />
+          <div className={styles.userStory}>
+            <div className={styles.userPic}>
+              <img src={PUBLIC_FOLDER + "/image17.jpg"} alt="" />
             </div>
+            <div className={styles.actionButton}> create Story</div>
           </div>
           <div className={styles.story}>
-            <div className={styles.profilePics}>
-              <img src="" alt="" />
+            <div className={styles.friendPic}>
+              <img src={PUBLIC_FOLDER + "/image7.jpg"} alt="" />
             </div>
+            <div className={styles.profilePics}>
+              <img src={PUBLIC_FOLDER + "/image10.jpg"} alt="" />
+            </div>
+            <span className="friendName">names</span>
           </div>
           <div className={styles.story}>
-            <div className={styles.profilePics}>
-              <img src="" alt="" />
+            <div className={styles.friendPic}>
+              <img src={PUBLIC_FOLDER + "/image17.jpg"} alt="" />
             </div>
-          </div>
+            <div className={styles.profilePics}>
+              <img src={PUBLIC_FOLDER + "/image11.jpg"} alt="" />
+            </div>
+            <span className="friendName">names</span>
+          </div>{" "}
           <div className={styles.story}>
-            <div className={styles.profilePics}>
-              <img src="" alt="" />
+            <div className={styles.friendPic}>
+              <img src={PUBLIC_FOLDER + "/image20.jpg"} alt="" />
             </div>
-          </div>
+            <div className={styles.profilePics}>
+              <img src={PUBLIC_FOLDER + "/image12.jpg"} alt="" />
+            </div>
+            <span className="friendName">names</span>
+          </div>{" "}
           <div className={styles.story}>
-            <div className={styles.profilePics}>
-              <img src="" alt="" />
+            <div className={styles.friendPic}>
+              <img src={PUBLIC_FOLDER + "/image1.jpg"} alt="" />
             </div>
-          </div>
+            <div className={styles.profilePics}>
+              <img src={PUBLIC_FOLDER + "/image15.jpg"} alt="" />
+            </div>
+            <span className="friendName">names</span>
+          </div>{" "}
           <div className={styles.story}>
-            <div className={styles.profilePics}>
-              <img src="" alt="" />
+            <div className={styles.friendPic}>
+              <img src={PUBLIC_FOLDER + "/image22.jpg"} alt="" />
             </div>
+            <div className={styles.profilePics}>
+              <img src={PUBLIC_FOLDER + "/image14.jpg"} alt="" />
+            </div>
+            <span className="friendName">names</span>
           </div>
         </div>
         <div
