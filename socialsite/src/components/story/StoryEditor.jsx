e3c95cf0e1672c0dee24f9579adfffbea17723a4
Tolulope Olofinjana <tolujana@gmail.com>
2023-08-03 23:57:14 +0100

    added story editor

---
+++ socialsite/src/components/story/StoryEditor.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const StoryEditor = () => {
+  return (
+    <div>
+      <div className="letmenu"></div>
+      <div className="maincontent">
+        <div className="uploadimage"></div>
+        <div className="addtext"></div>
+      </div>
+    </div>
+  );
+};
+
+export default StoryEditor;
