cb0d1830d2706fa826e14e8699d97482e36d3643
Tolulope Olofinjana <tolujana@gmail.com>
2023-08-08 00:02:18 +0100

    created more helper function

--- socialsite/src/utils/generalServices.js
+++ socialsite/src/utils/generalServices.js
@@ -1,3 +1,36 @@
+import { axiosInstance } from "../proxySettings";
+
 export const openPopupDialog = (action, dispatch) => {
   dispatch(action);
+};
+
+export const confirmFriendRequest = async (userId, currentUser, dispatch) => {
+  try {
+    const res = await axiosInstance.put(`users/${userId}/friend`, {
+      userId: currentUser._id,
+    });
+    if (res.status === 200) {
+      dispatch({
+        type: "UPDATE_FRIEND",
+        payload: { ...currentUser, friends: res.data },
+      });
+    }
+  } catch (error) {}
+};
+
+export const cancelFriendRequest = async (userId, currentUser, dispatch) => {
+  try {
+    const res = await axiosInstance.put(`users/${userId}/cancelrequest`, {
+      id: currentUser._id,
+    });
+
+    if (res.status === 200) {
+      dispatch({
+        type: "UPDATE_FRIENDREQUEST",
+        payload: { ...currentUser, friendRequest: res.data },
+      });
+    }
+  } catch (error) {
+    console.log(error.message);
+  }
 };
