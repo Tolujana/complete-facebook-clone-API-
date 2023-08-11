23fc2864a258f6cf6f67e06fc4070f92a086c8a4
Tolulope Olofinjana <tolujana@gmail.com>
2023-08-11 14:33:18 +0100

    updated services

--- socialsite/src/utils/generalServices.js
+++ socialsite/src/utils/generalServices.js
@@ -14,23 +14,65 @@ export const confirmFriendRequest = async (userId, currentUser, dispatch) => {
         type: "UPDATE_FRIEND",
         payload: { ...currentUser, friends: res.data },
       });
+      return true;
     }
   } catch (error) {}
 };
 
-export const cancelFriendRequest = async (userId, currentUser, dispatch) => {
+export const DeleteFriendRequest = async (userId, currentUser) => {
   try {
     const res = await axiosInstance.put(`users/${userId}/cancelrequest`, {
       id: currentUser._id,
     });
 
     if (res.status === 200) {
-      dispatch({
-        type: "UPDATE_FRIENDREQUEST",
-        payload: { ...currentUser, friendRequest: res.data },
-      });
+      return true;
     }
   } catch (error) {
     console.log(error.message);
   }
+};
+
+export const confirmFriend = (userId, currentUser, dispatch, setButtonText) => {
+  const isConfirmed = confirmFriendRequest(userId, currentUser, dispatch);
+
+  if (isConfirmed) {
+    DeleteFriendRequest(userId, currentUser);
+    setButtonText("Friends");
+  }
+};
+
+// export const handleFiless = (files, fromDragnDrop = false) => {
+//   console.log(files);
+//   if (fromDragnDrop && !checkFileIsValid(files)) setError("file/file(s)  not an image");
+
+//   const data = new FormData();
+//   const filesArray = Object.values(files);
+//   let fileNames = [];
+//   filesArray.forEach((file) => {
+//     const fileName = Date.now() + file.name;
+//     fileNames = [...fileNames, fileName];
+//     data.append("files", file, fileName);
+//   });
+//   setFileNames(fileNames);
+//   setUploadFiles(data);
+//   setDisplayData(filesArray);
+// };
+
+export const handleFiles = (files, fromDragnDrop = false, checkFile) => {
+  let errorMessage = "";
+  if (fromDragnDrop && !checkFile(files)) {
+    errorMessage = "file/file(s)  not an image";
+  }
+
+  const data = new FormData();
+  const filesArray = Object.values(files);
+  let fileNames = [];
+  filesArray.forEach((file) => {
+    const fileName = Date.now() + file.name;
+    fileNames = [...fileNames, fileName];
+    data.append("files", file, fileName);
+  });
+
+  return [fileNames, data, filesArray, errorMessage];
 };
