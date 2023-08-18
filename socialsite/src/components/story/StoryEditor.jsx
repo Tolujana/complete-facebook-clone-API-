2a785b49a21757c689ebee71a2d6e0752f70b6c7
Tolulope Olofinjana <tolujana@gmail.com>
2023-08-18 23:11:26 +0100

    updated design, added functionality

--- socialsite/src/components/story/StoryEditor.jsx
+++ socialsite/src/components/story/StoryEditor.jsx
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import styles from "./storyeditor.module.css";
 import Topmenu from "../topmenu/topmenu";
+import { handleFiles, processDragNDrop } from "../../utils/generalServices";
+import { AuthContext } from "../../context/AuthContext";
+
 const StoryEditor = () => {
+  const [isDragActive, setDragActive] = useState(false);
+  const [uploadData, setUPloadData] = useState(null);
+  const [files, setFiles] = useState("");
+  const { user } = useContext(AuthContext);
+  const handleFileUpload = (event) => {
+    const dropActive = event.type === "drop" ? true : false;
+    const files = dropActive ? event.dataTransfer.files : event.target.files;
+    const [fileNames, data, filesArray, errorMessage] = handleFiles(files, dropActive);
+
+    setFiles(filesArray);
+    setDragActive(true);
+    setUPloadData(data);
+    console.log(event);
+  };
+
+  const newStory = {
+    userId: user._id,
+    files: files,
+  };
   return (
     <>
       <Topmenu />
@@ -11,10 +33,62 @@ const StoryEditor = () => {
             <h1 className={styles.title}>Your Story</h1>
             <div className={styles.profileImage}></div>
           </div>
+          {isDragActive && (
+            <div className={styles.createStory}>
+              <button className={styles.discard}>Discard</button>
+              <button
+                onclick={(e) => {
+                  uploadData("story", uploadData);
+                }}
+                className={styles.shareStory}
+              >
+                Share to Story{" "}
+              </button>
+            </div>
+          )}
         </div>
-        <div className={styles.maincontent}>
-          <div className={styles.uploadimage}>Create a Photo story</div>
-          <div className={styles.addtext}>Create a Text Story</div>
+        <div className={styles.outerContainer}>
+          {!isDragActive ? (
+            <div className={styles.maincontent}>
+              <input
+                type="file"
+                name="files"
+                hidden
+                id="fileInput"
+                accept="image/png, image/gif, image/jpeg,video/mp4"
+                className={styles.fileUpload}
+                onChange={handleFileUpload}
+              />
+
+              <label
+                htmlFor="fileInput"
+                className={styles.uploadimage}
+                onDrop={(e) => {
+                  setDragActive(true);
+                  processDragNDrop(e, setDragActive, handleFileUpload);
+                }}
+                onDragEnter={(e) => {
+                  processDragNDrop(e, setDragActive, handleFileUpload);
+                }}
+                onDragOver={(e) => {
+                  processDragNDrop(e, setDragActive, handleFileUpload);
+                }}
+                onDragLeave={(e) => {
+                  processDragNDrop(e, setDragActive, handleFileUpload);
+                }}
+              >
+                <div> Create a Photo story</div>
+              </label>
+
+              <div className={styles.addtext}>Create a Text Story</div>
+            </div>
+          ) : (
+            <div className={styles.story}>
+              <div className={styles.storyImageContainer}>
+                <img src={URL.createObjectURL(files[0])} alt="" className={styles.storyImage} />
+              </div>
+            </div>
+          )}
         </div>
       </div>
     </>
