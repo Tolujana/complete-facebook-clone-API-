661bfd6d2a068dc36955f65d8cb36d326186e556
Tolulope Olofinjana <tolujana@gmail.com>
2023-08-24 22:52:34 +0100

    updated profile methods

--- socialsite/src/utils/profileServices.js
+++ socialsite/src/utils/profileServices.js
@@ -1,8 +1,43 @@
 import { axiosInstance } from "../proxySettings";
 
-export const updateFriendship = async (user, friendRequests, currentUserId, setButtonText) => {
+export const hasFriendRequest = (userRequest, recipientID) => {
+  return userRequest.some((request) => request.id === recipientID);
+};
+
+// export const updateFriendship = async (user, currentUserFriendRequests, currentUserId, setButtonText) => {
+//   try {
+
+//     if (!currentUserFriendRequests.includes(user._id) && !user?.friends?.includes(currentUserId)) {
+//       const data = { id: currentUserId };
+//       const res = await axiosInstance.put("/users/" + user._id + "/request", data);
+//       // console.log("step one", res.data);
+//       setButtonText(res.data);
+//     }
+
+//     if (user.friends?.includes(currentUserId)) {
+//       const data = { id: currentUserId };
+//       const res = await axiosInstance.put("/users/" + user._id + "/unfriend", data);
+//       console.log("ia in");
+//       setButtonText("Add Friend");
+//     }
+//   } catch (err) {}
+// };
+
+export const sendMessage = async (data, chats, dispatch) => {
+  if (!chats.includes(data)) {
+    dispatch({ type: "CHAT_START", payload: data });
+  }
+};
+
+export const updateFriendship = async (
+  user,
+  currentUserFriendRequests,
+  currentUserId,
+  setButtonText
+) => {
   try {
-    if (!friendRequests.includes(user._id) && !user?.friends?.includes(currentUserId)) {
+    const hasRequestbeensent = hasFriendRequest(currentUserFriendRequests, user._id);
+    if (!hasRequestbeensent && !user?.friends?.includes(currentUserId)) {
       const data = { id: currentUserId };
       const res = await axiosInstance.put("/users/" + user._id + "/request", data);
       // console.log("step one", res.data);
@@ -16,10 +51,4 @@ export const updateFriendship = async (user, friendRequests, currentUserId, setB
       setButtonText("Add Friend");
     }
   } catch (err) {}
-};
-
-export const sendMessage = async (data, chats, dispatch) => {
-  if (!chats.includes(data)) {
-    dispatch({ type: "CHAT_START", payload: data });
-  }
 };
