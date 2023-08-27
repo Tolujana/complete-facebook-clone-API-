2c071b5379606a0ff0f10fe99f3c476343b936ce
Tolulope Olofinjana <tolujana@gmail.com>
2023-08-28 00:24:52 +0100

    updated base url

--- socialsite/src/proxySettings.js
+++ socialsite/src/proxySettings.js
@@ -1,8 +1,8 @@
 import axios from "axios";
-
+API = process.env.REACT_APP_BASEURL;
 export const axiosInstance = axios.create({
   // baseURL: 'https://socialmedia-site.herokuapp.com/api/',
-  baseURL: "http://localhost:8800/api",
+  baseURL: API,
   // validateStatus: function (status) {
   //   return status >= 200 && status < 300; // default
   // },
